3f891239de815ccb2f6d9674886edbeebccf5d3d
Douglas Schreck <dcschreck421@gmail.com>
2018-07-21 21:33:31 +0300

    configuring create_message

--- lib/kele.rb
+++ lib/kele.rb
@@ -5,7 +5,7 @@ require 'kele/roadmap'
 class Kele
     include HTTParty
     include Roadmap
-    
+
     base_uri = 'https://www.bloc.io/api/v1'
 
     def initialize(email, password)
@@ -32,7 +32,7 @@ class Kele
 
     def get_mentor_availability(mentor_id)
         available = []
-        response = self.class.get(build_url('/mentors/'+"#{mentor_id}"+'/student_availability'), headers: {"authorization" => @auth_token })
+        response = self.class.get(build_url('/mentors/' + "#{mentor_id}" + '/student_availability'), headers: {"authorization" => @auth_token })
         schedule = JSON.parse(response.body)
         schedule.each do |session|
             if session['booked'] == nil
@@ -40,6 +40,32 @@ class Kele
             end
         end
         return available
+    end
+
+    def get_messages(page = nil)
+        if page != nil
+            response = self.class.get(build_url('/message_threads'), headers: {"authorization" => @auth_token}, body: {page: page})
+        else
+            response = self.class.get(build_url('/message_threads'), headers: {"authorization" => @auth_token})
+        end
+        JSON.parse(response.body)
+    end
+
+    def create_message(sender, recipient_id, token = nil, subject = nil, stripped_text)
+        message_options = {
+            sender: sender,
+            recipient_id: recipient_id,
+            token: token,
+            subject: subject,
+            stripped_text: stripped_text
+        }
+
+        response = self.class.post(build_url('/messages'), headers: {"authorization" => @auth_token}, body: message_options)
+        if response.success?
+            p "Message sent"
+        else
+            p "Message not sent"
+        end
     end
 
     private
--- lib/kele/roadmap.rb
+++ lib/kele/roadmap.rb
@@ -8,5 +8,6 @@ module Roadmap
 
     def get_checkpoint(checkpoint_id)
         response = self.class.get(build_url('/checkpoints/'+"#{checkpoint_id}"), headers: {"authorization" => @auth_token })
+        JSON.parse(response.body)
     end
 end
